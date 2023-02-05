99852ce3bb0369dfa8d4de1cd8a2bd5961e01f23
Stepan Vovk <stepan.vovk@plvision.eu>
2023-02-05 20:26:30 +0200

    Add clan uknown unicast test Signed-off-by: Stepan Vovk <stepan.vovk@plvision.eu> on.eu>

--- DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/functional/vlan/test_vlan_default_configuration.py
+++ DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/functional/vlan/test_vlan_default_configuration.py
@@ -318,3 +318,99 @@ async def test_vlan_default_configuration_with_unicast(testbed):
         else:
             assert tgen_utils_get_loss(row) == 100.000, \
                 f'Traffic leak for traffic item: {row["Traffic Item"]} on port {row["Rx Port"]}'
+
+
+async def test_vlan_default_configuration_unknown_unicast(testbed):
+    """
+    Test Name:VLAN with unknown unicast
+    Test Suite: suite_functional_vlan
+    Test Overview: Test VLAN  unknown unicast  packet forwarding
+    Test Procedure:
+    1. Initiate test params.
+    2. Map receiving and non receiving dut_ports.
+        Port 1 ->  tx_port
+        Ports 2, 3, 4 -> rx_ports
+    3. Setup packet stream(s) for the broadcast packet:
+    4. Send traffic to rx_ports
+    5. Verify traffic on rx ports
+    """
+
+    # 1. Initiate test params.
+    tgen_dev, dent_devices = await tgen_utils_get_dent_devices_with_tgen(testbed, [], 4)
+    if not tgen_dev or not dent_devices:
+        print("The testbed does not have enough dent with tgn connections")
+        return
+    device = dent_devices[0].host_name
+    tg_ports = tgen_dev.links_dict[device][0]
+    dut_ports = tgen_dev.links_dict[device][1]
+
+    out = await IpLink.add(input_data=[{device: [{
+        "device": "br0",
+        "type": "bridge",
+        "vlan_filtering": 1
+        }]
+    }])
+    assert out[0][device]["rc"] == 0, f"Failed creating bridge."
+
+    await IpLink.set(input_data=[{device: [{"device": "br0", "operstate": "up"}]}])
+    assert out[0][device]["rc"] == 0, f"Failed setting bridge to state UP."
+
+    out = await IpLink.set(input_data=[{device: [{
+        "device": port,
+        "operstate": "up",
+        "master": "br0"
+    } for port in dut_ports]}])
+    assert out[0][device]["rc"] == 0, f"Failed setting link to state UP."
+
+    # 2.Map receiving and non receiving dut_ports.
+    dev_groups = tgen_utils_dev_groups_from_config([
+        {"ixp": tg_ports[0], "ip": "100.1.1.2", "gw": "100.1.1.6", "plen": 24, },
+        {"ixp": tg_ports[1], "ip": "100.1.1.3", "gw": "100.1.1.6", "plen": 24, "vlan": 1},
+        {"ixp": tg_ports[2], "ip": "100.1.1.4", "gw": "100.1.1.6", "plen": 24, },
+        {"ixp": tg_ports[3], "ip": "100.1.1.5", "gw": "100.1.1.6", "plen": 24, }])
+    await tgen_utils_traffic_generator_connect(tgen_dev, tg_ports, dut_ports, dev_groups)
+
+    tx_ports = dev_groups[tg_ports[0]][0]["name"]
+    rx_ports = [dev_groups[tg_ports[1]][0]["name"],
+                dev_groups[tg_ports[2]][0]["name"],
+                dev_groups[tg_ports[3]][0]["name"]]
+
+    # 3. Setup packet stream(s).
+    # Tagged stream
+    streams = {f"{tg_ports[0]} -> with VLAN: {vlan}": {
+        "type": "raw",
+        "protocol": "802.1Q",
+        "ip_source": tx_ports,
+        "ip_destination": rx_ports,
+        "srcMac": "02:00:00:00:00:01",
+        "dstMac": "02:00:00:00:00:02",
+        "vlanID": vlan
+    } for vlan in packet_vids if vlan != "X"}
+
+    # Untagged stream
+    streams.update({"Untagged stream": {
+        "type": "raw",
+        "protocol": "802.1Q",
+        "ip_source": tx_ports,
+        "ip_destination": rx_ports,
+        "srcMac": "02:00:00:00:00:01",
+        "dstMac": "02:00:00:00:00:02",
+    }})
+
+    await tgen_utils_setup_streams(tgen_dev, config_file_name=None, streams=streams)
+
+    # 4. Send traffic to rx_ports.
+    await tgen_utils_start_traffic(tgen_dev)
+    await asyncio.sleep(5)
+    await tgen_utils_stop_traffic(tgen_dev)
+
+    # 5. Verify traffic.
+    stats = await tgen_utils_get_traffic_stats(tgen_dev, "Flow Statistics")
+    ti_to_rx_port_map = get_traffic_port_vlan_mapping(streams, port_map, tg_ports)
+    for row in stats.Rows:
+        if row["Rx Port"] in ti_to_rx_port_map[row["Traffic Item"]]:
+            assert tgen_utils_get_loss(row) == 0.000, \
+                f'Traffic leak for traffic item: {row["Traffic Item"]} on port {row["Rx Port"]}'
+        else:
+            assert tgen_utils_get_loss(row) == 100.000, \
+                f'No traffic for traffic item : {row["Traffic Item"]} on port {row["Rx Port"]}'
