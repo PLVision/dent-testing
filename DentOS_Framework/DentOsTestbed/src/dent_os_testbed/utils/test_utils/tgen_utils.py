cc5d962435147fa291fdfef26ab21799b23c86e8
Kostiantyn Stavruk <kostiantyn.stavruk@plvision.eu>
2023-02-21 16:15:01 +0200

    add functionality which clear traffic items
    Signed-off-by: Kostiantyn Stavruk <kostiantyn.stavruk@plvision.eu>

--- DentOS_Framework/DentOsTestbed/src/dent_os_testbed/utils/test_utils/tgen_utils.py
+++ DentOS_Framework/DentOsTestbed/src/dent_os_testbed/utils/test_utils/tgen_utils.py
@@ -486,6 +486,11 @@ async def tgen_utils_clear_traffic_stats(device):
     device.applog.info(out)
     # assert out[0][device.host_name]["rc"] == 0
 
+async def tgen_utils_clear_traffic_items(device):
+    device.applog.info("Clearing Traffic Items")
+    out = await TrafficGen.clear_traffic(input_data=[{device.host_name: [{}]}])
+    device.applog.info(out)
+    # assert out[0][device.host_name]["rc"] == 0
 
 async def tgen_utils_stop_protocols(device):
     device.applog.info("Stopping Protocols")
